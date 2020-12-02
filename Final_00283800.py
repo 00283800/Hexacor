e46df6bc897fbb755a85e3a8267f5745bd35a3a5
00283800 <72091396+00283800@users.noreply.github.com>
2020-12-02 08:44:05 -0500

    Add files via upload

--- Final_00283800.py
+++ Final_00283800.py
@@ -1,8 +1,12 @@
 #Matt Webster
 #Programming for IT CIS-153
 #Final Project
-#
+#Hexacor, text-based role playing game
 
+#allows import of system command to clear screen from python
+from os import system
+
+#title
 print(" ____        ____     ________________    ____                  ____            ______                  _______________      ________________      __________________ ")
 print("|    |      |    |   |                |   \    \               /   /           |      |                /               |    |                |    |                  \ ")
 print("|    |      |    |   |                |    \    \             /   /            |      |               /                |    |                |    |     ________      \ ")
@@ -21,4 +25,26 @@ print("|    |      |    |   |     |__________       /    /         \    \       
 print("|    |      |    |   |                |     /    /           \    \     |    |         |    |        \                 |    |                |    |     |             \     \ ")
 print("|    |      |    |   |                |    /    /             \    \    |    |         |    |         \                |    |                |    |     |              \     \ ")
 print("|____|      |____|   |________________|   /____/               \____\   |____|         |____|          \_______________|    |________________|    |_____|               \_____\ ")
+
+#start game
+print("")
+print("")
+input("Press enter to begin")
+
+#clear title after beginning
+system("cls")
+
+#intro
+print("")
+print("")
+print("                  |")
+print("                  |")
+print("                  |")
+print("                  |")
+print("                  |")
+print("                  |")
+print("")
+print("")
+print("You are but a single line in a land full of shapes.\nSome day you hope to prove yourself and show that you're not just another one-dimensional being....")
+hname = input("Now...what is your name?\n")
 input()
